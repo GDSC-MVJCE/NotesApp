2c6df86aff7446001e6cb4337c6d68fee45f1322
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2023-12-27 18:02:04 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.notesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import com.example.notesapp.ui.theme.NotesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(topBar = {
+        TopAppBar(title = { Text("Notevault") })
+    }) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(12.dp)
+                ) {
+                    Text("Homework", fontSize = 5.em)
+                    Text("Hello world. This is a note")
+                }
+            }
+
+
+        }
+
+    }
+}
