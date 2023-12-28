36b2dfe78ea06c8d0cbd08074c17b561c10f2e5b
Sidharth Sreejil <94698713+sid-js@users.noreply.github.com>
2023-12-28 11:03:24 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.notesapp
+package com.example.notetakingapp
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,25 +8,32 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
-import com.example.notesapp.ui.theme.NotesAppTheme
+import com.example.notetakingapp.ui.theme.NoteTakingAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NotesAppTheme {
+            NoteTakingAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -40,28 +48,53 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen() {
-    Scaffold(topBar = {
-        TopAppBar(title = { Text("Notevault") })
-    }) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
+fun MainScreen(){
+    val notes = remember {
+        mutableStateListOf<NoteModel>(
+            NoteModel("Title","Content"),
+            NoteModel("Workshop","Hello World")
+        )
+    }
 
-            ) {
-                Column(
-                    modifier = Modifier
-                        .padding(12.dp)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+               title = {
+                   Text("NoteVault")
+               }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+
+                }
+            ){
+                Text("Add")
+            }
+        }
+
+    ){
+        innerPadding ->
+        LazyColumn(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            items(notes.toList()) {
+                it ->
+                Card(
+                    modifier = Modifier.padding(16.dp)
+                        .fillMaxWidth()
                 ) {
-                    Text("Homework", fontSize = 5.em)
-                    Text("Hello world. This is a note")
+                    Column {
+                        Text(it.title, fontSize = 5.em)
+                        Text(it.content)
+                    }
                 }
             }
-
-
         }
 
     }
 }
+
+
+
